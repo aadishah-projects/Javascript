e09b0fa65e68935a0781fa973a0e6caa1f14a8f7
aadishah-projects <adityakshah69@gmail.com>
2024-12-03 23:43:00 +0545

    setting up node js

---
+++ new.js
@@ -0,0 +1,2 @@
+console.log("Hey I am console")
+console.log("Code is running")
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 alert("hello World")
+
+
+var a = prompt("Enter a number ")
+var isTrue = confirm("Are u sure u want to leave the site")
+
+if (isTrue)
+{
+    console.log("QUITINNG>>>>>>")
+}
+else
+{
+    console.log("HELLLLLLLPPPPPPPPPPP")
+    }
+console.log(a)
