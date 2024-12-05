306eadd13e4036064b99c7f31391d216de8cd8c4
aadishah-projects <adityakshah69@gmail.com>
2024-12-05 19:23:37 +0545

    Variables in Javascript

--- intro/script.js
+++ intro/script.js
@@ -13,3 +13,11 @@ else
     console.log("HELLLLLLLPPPPPPPPPPP")
     }
 console.log(a)
+
+document.title = "aadi-codes"
+// document.body.style.backgroundColor = 'red'
+
+var a = 5
+var b = 6
+
+console.log(a+ b)
---
+++ variables/script.js
@@ -0,0 +1,38 @@
+var a = 5; // global scope
+var b = 6;
+
+{
+    let a = 7 // block scope
+    console.log("the val of a is: " + a)
+}
+console.log("(Outside )the val of a is: " + a)
+
+const c = 7
+// c = c + 1 not allowed 
+console.log(c)
+
+
+console.log(a + b)
+console.log(typeof(a))
+console.log(typeof (b))
+{
+    // Seven primitive types
+    let x = "Aaditya"
+    let y = 45
+    let z = 3.14
+    let b = true
+    let f = undefined
+    let r = null
+
+    console.log(typeof (x), typeof (y), typeof (z), typeof (b), typeof (f), typeof (r)) //type of null is  " object "
+}
+
+{
+    let obj = {
+        'name': "aaditya",
+        'age': 19
+    }
+    console.log(obj) // like a dictionary in python
+    obj.salary = 90 //appending in javascript
+    console.log(obj) 
+}
