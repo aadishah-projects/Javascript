827abc16907c2fa4beb187bacdf1f79ccfba6065
aadishah-projects <adityakshah69@gmail.com>
2024-12-09 19:49:30 +0545

    Strings

---
+++ 7_strings/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+let first_name = "aditya"
+let last_name = "Shah"
+
+console.log("My full  name is : " + first_name + last_name)
+
+// Similar to fromat f"" in python here is : ``
+console.log(`Hello my name is: ${first_name} ${last_name}`)
+
+let b = "Text1"
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.replace("ex", "12"))
+console.log(b.concat(first_name))
