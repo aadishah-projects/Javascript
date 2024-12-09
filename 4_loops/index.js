2f0b45ab9efa4c82f75a4166e296c8a59cf0da53
aadishah-projects <adityakshah69@gmail.com>
2024-12-09 07:57:49 +0545

    loops

--- 4_loops/index.js
+++ 4_loops/index.js
@@ -1,5 +1,5 @@
 console.log(" I am on loops")
-
+// for loops
 {
     let a = 1
     for (let i = 10; i >= 1; i--)
@@ -7,6 +7,34 @@ console.log(" I am on loops")
         console.log(i)
     }
 }
+//for in and forof loops 
 {
-    console.log("Test")
+    let obj = {
+        'name': "Aditya",
+        "age": 21,
+        "gender": 'male'
+    }
+    for (const key in obj) {
+        const element = obj[key]
+        console.log(key,element)
+    }
+    //for of loops
+    for (const element of "Aadi") {
+        console.log(element)
+    }
+}
+
+//while loops
+{
+    let i = 1
+    while (i <= 6)
+    {
+        console.log(i)
+        i++
+    }
+     i = 1
+    do {
+        console.log(i)
+        i++
+    }while(i<=6)
 }
