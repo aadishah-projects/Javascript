101eb44101161529f0beac4e7b0512a42a5f201e
aadishah-projects <adityakshah69@gmail.com>
2024-12-09 08:04:48 +0545

    functions

---
+++ 5_functions/index.js
@@ -0,0 +1,15 @@
+
+function naming(name)
+{
+    console.log("Hello!", name)
+}
+naming("Additya")
+
+
+function sum(a, b, c = 9)
+{
+    return a+ b  + c 
+}
+console.log(sum(3, 5,1)) 
+
+console.log("The result of the sum is: " + sum(7,98))
---
+++ 5_functions/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+function sum(a, b, c = 9)
+{
+    return a+ b  + c 
+}
+console.log(sum(3, 5,1)) 
+
+console.log("The result of the sum is: " + sum(7,98))
