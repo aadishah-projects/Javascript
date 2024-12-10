c7e7ffb2c50354f2f40382b4281b8d201f18040b
aadishah-projects <adityakshah69@gmail.com>
2024-12-10 23:59:40 +0545

    DOM-ParentChild

---
+++ 9_DOM/scripts.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+document.body.childNodes[0]
+document.body.childNodes[1]
+document.body.childNodes[5]
+let container = document.body.childNodes[5]
+container.firstElementChild.style.backgroundColor = "green"
+container.lastElementChild.parentElement
+document.body.childNodes[5]
+document.body.childNodes[5].children
+document.body.childNodes[5].children[3].nextElementSibling
